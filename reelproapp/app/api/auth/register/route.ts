44b938aa14f231bde408471b19b591c465c6fc68
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-05-23 13:09:38 +0530

    RegisterAPI

---
+++ reelproapp/app/api/auth/register/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest,NextResponse } from "next/server";
+import { dbConnect } from "@/app/lib/db";
+import {User} from "@/models/User.model.ts";
+
+export async function POST(request: NextRequest){
+    try {
+        const {email,password} = await request.json();
+
+        if(!email || !password){
+            return NextResponse.json(
+                {error: "Email and password are required"},
+                {status: 400}
+            );
+        }
+
+        await dbConnect();
+        
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return NextResponse.json(
+                {error: "User already exists"},
+                {status: 400}
+            );
+        }
+
+        await User.create({
+            email,
+            password,
+        })
+
+        return NextResponse.json(
+            {message: "User Registered Sucessfully"},
+            {status: 201}
+        );
+    } catch (error) {
+        console.log("Register Error",error);
+        return NextResponse.json(
+            {error:"Failed to register user"},
+            {status: 500}
+        );
+    }
+}
