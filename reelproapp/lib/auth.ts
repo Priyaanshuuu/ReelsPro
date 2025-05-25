651c32c868ac36516907c8433a7510baf7bd4d78
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-05-25 18:05:37 +0530

    Auth Providers

---
+++ reelproapp/lib/auth.ts
@@ -0,0 +1,51 @@
+import { NextAuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { dbConnect } from "./db";
+import  User  from "../models/User.model";
+
+export const authOptions : NextAuthOptions= {
+    providers: [
+        CredentialsProvider({
+            name:"Credentials",
+            credentials:{
+                email: {label:"Email",type:"text"},
+                password: {label:"Password",type:"password"},
+            },
+            async authorize(credentials){
+                if(!credentials?.email || credentials?.password){
+                    throw new Error("Missing email or password");
+                }
+
+                try {
+                    await dbConnect();
+                    const user = await User.findOne({email:credentials.email});
+
+                    if(!user){
+                        throw new Error("No user found with this email");
+                    }
+
+                    const isValid = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    );
+
+                    if(!isValid){
+                        throw new Error("Invalid Password")
+                    }
+
+                    return {
+                        id: user._id.toString(),
+                        email: user.email,
+                    };
+                } catch (error) {
+                    console.error("Auth Error",error);
+                    throw error;
+                    
+                }
+            }
+
+        })
+    ],
+    
+}
