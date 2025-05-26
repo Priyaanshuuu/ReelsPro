79f2b51e0479959108c95b034be547099347a800
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-05-27 00:29:18 +0530

    middleware

---
+++ reelproapp/middleware.ts
@@ -0,0 +1,13 @@
+import {withAuth} from "next-auth/middleware"
+import { NextResponse} from "next/server"
+
+export default withAuth(
+    function middleware(){
+        return NextResponse.next();
+    },
+    {
+        callbacks:{
+            
+        }
+    }
+)
