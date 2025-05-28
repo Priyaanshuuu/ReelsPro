fe55b01cd89726ca2722bff099a89347d1181ce4
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-05-28 12:56:09 +0530

    middleware

--- reelproapp/middleware.ts
+++ reelproapp/middleware.ts
@@ -7,7 +7,29 @@ export default withAuth(
     },
     {
         callbacks:{
-            
-        }
+            authorized:({token,req})=>{
+                const {pathname}= req.nextUrl;
+
+                if(
+                    pathname.startsWith("/api/auth")||
+                    pathname === "/login" ||
+                    pathname === "/register"
+                ){
+                    return true;
+                }
+
+                if(pathname==="/"|| pathname.startsWith("/api/videos")){
+                    return true;
+                }
+                return !!token
+            },
+        },
     }
-)
+);
+
+export const config = {
+    matcher: [
+        "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+
+    ]
+}
