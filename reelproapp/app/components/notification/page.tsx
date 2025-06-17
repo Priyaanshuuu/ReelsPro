d676757a358c0e555ecbcd89f136ba579985e1a9
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-06-17 20:42:26 +0530

    notification

--- reelproapp/app/components/notification/page.tsx
+++ reelproapp/app/components/notification/page.tsx
@@ -1,8 +1,33 @@
-export default function NotificationProvider() {
-    return (
-        <div>
-            <h1>Notifications</h1>
-            {/* Render your notification content here */}
-        </div>
-    );
+"use client";
+
+import { createContext, useContext, useState, ReactNode } from "react";
+
+type NotificationType = "success" | "Error"| "info"| "warning";
+
+interface NotificationContextType{
+    showNotification:(
+        message: string,
+        type: NotificationType,
+    )=> void;
+}
+
+const NotificationContext = createContext<NotificationContextType | undefined>(
+    undefined
+);
+
+export function NotificationProvider({ children }: { children: ReactNode }) {
+    const [notification, setNotification] = useState<{
+        message: string,
+        type: NotificationType;
+        id: number;
+    }| null>(null);
+
+    const showNotification = (message: string , type : NotificationType)=>{
+        const id = Date.now();
+        setNotification({ message, type, id });
+        setTimeout(()=>{
+            setNotification((current)=>(current?.id === id? null: current));
+        }, 3000);
+        }
+    }
 }
