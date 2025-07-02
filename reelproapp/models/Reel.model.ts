4fa59305ffd90c2d2d878f142d666524bdd61157
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-07-02 19:40:42 +0530

    ReelSchema

---
+++ reelproapp/models/Reel.model.ts
@@ -0,0 +1,35 @@
+import   { Schema, models, model } from "mongoose";
+
+const ReelSchema = new Schema(
+  {
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      default: "",
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isPrivate: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Reel = models.Reel || model("Reel", ReelSchema);
+export default Reel;
